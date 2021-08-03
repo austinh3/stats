cbf64ee10531ecc52a35cd5ab05f46a7f5536846
Bill <bill@bcorrigan.com>
2021-08-02 20:49:23 -0500

    set security for access to storage bucket with cors.json

--- public/app.js
+++ public/app.js
@@ -71,6 +71,39 @@
   };
 }
 
+var auth = firebase.auth();
+var storageRef = firebase.storage().ref();
+// Create a reference to the file we want to download
+var trailsRef = storageRef.child('trails/index.html');
+
+// Get the download URL
+trailsRef.getDownloadURL()
+.then((url) => {
+  // Insert url into an <a> tag to "download"
+    document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">Proceed to Stats and maps</a>';
+})
+.catch((error) => {
+  // A full list of error codes is available at
+  // https://firebase.google.com/docs/storage/web/handle-errors
+  switch (error.code) {
+    case 'storage/object-not-found':
+      // File doesn't exist
+      break;
+    case 'storage/unauthorized':
+      // User doesn't have permission to access the object
+      break;
+    case 'storage/canceled':
+      // User canceled the upload
+      break;
+
+    // ...
+
+    case 'storage/unknown':
+      // Unknown error occurred, inspect the server response
+      break;
+  }
+});
+
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 // Disable auto-sign in.
